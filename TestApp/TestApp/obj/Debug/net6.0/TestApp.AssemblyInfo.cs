d7c9647eb9c8bfcb9a86344391cb115c22c188a8
Shipochki <121862582+Shipochki@users.noreply.github.com>
2024-03-13 11:22:50 +0200

    Tim added text

--- TestApp/TestApp/obj/Debug/net6.0/TestApp.AssemblyInfo.cs
+++ TestApp/TestApp/obj/Debug/net6.0/TestApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b67fa556d79b1ed7c2eebbfbbe310ec4211597c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
